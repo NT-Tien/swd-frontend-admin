457d8e2903b4f2a0ef718c15f03fb50749bd1ef7
Sang Dang <sangdase170287@fpt.edu.vn>
2024-01-13 14:08:25 +0700

    Prettier Reformat

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import env from "./env";
+import { initializeApp } from 'firebase/app'
+import env from './env'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: env.SWD_FB_API_KEY(),
-  authDomain: env.SWD_FB_AUTH_DOMAIN(),
-  projectId: env.SWD_FB_PROJECT_ID(),
-  storageBucket: env.SWD_FB_STORAGE_BUCKET(),
-  messagingSenderId: env.SWD_FB_MESSAGING_SENDER_ID(),
-  appId: env.SWD_FB_APP_ID(),
-};
+    apiKey: env.SWD_FB_API_KEY(),
+    authDomain: env.SWD_FB_AUTH_DOMAIN(),
+    projectId: env.SWD_FB_PROJECT_ID(),
+    storageBucket: env.SWD_FB_STORAGE_BUCKET(),
+    messagingSenderId: env.SWD_FB_MESSAGING_SENDER_ID(),
+    appId: env.SWD_FB_APP_ID(),
+}
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
